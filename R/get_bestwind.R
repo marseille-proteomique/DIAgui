37f8f289fa4b44b62cf9d1d3aa4a86d4994ce579
mgerault <67689583+mgerault@users.noreply.github.com>
2023-11-17 12:11:51 +0100

    Add files via upload

--- R/get_bestwind.R
+++ R/get_bestwind.R
@@ -20,7 +20,8 @@
 #'
 #' @export
 
-get_bestwind <- function(data, n_window = 25, per_frac = FALSE, window_size = NULL){
+get_bestwind <- function(data, n_window = 25, per_frac = FALSE,
+                         overlap = NULL, window_size = NULL){
   if(inherits(data, "character")){
     if(stringr::str_detect(data, "\\.tsv$"))
       data <-  diann_load(data)
@@ -97,34 +98,36 @@ get_bestwind <- function(data, n_window = 25, per_frac = FALSE, window_size = NU
           config = append(config, nb)
         }
 
-        wind <- c()
-        for(k in 1:n_window){ # get the windows --> same amount for each
-          if(k == 1){
-            dep = 1
+        config <- c(0, cumsum(config))
+        un_wind <- list()
+        for(k in 1:(length(config) - 1)){
+          wind <- range(data_bis$PrecursorMz[(config[k]+1):config[k+1]], na.rm = TRUE)
+          wind[1] <- floor(wind[1])
+          wind[2] <- ceiling(wind[2])
+          un_wind[[k]] <- wind
+        }
+        for(k in 1:(length(un_wind) - 1)){
+          if(un_wind[[k]][2] == un_wind[[length(un_wind)]][2]){
+            un_wind[[k]][2] <- un_wind[[length(un_wind)]][1]
           }
-          else{
-            dep = sum(config[1:(k-1)])
+          if(un_wind[[k]][2] != un_wind[[k+1]][1]){
+            un_wind[[k+1]][1] <- un_wind[[k]][2]
           }
-          val = data_bis$PrecursorMz[dep:sum(config[1:k])]
-          wind <- append(wind, round(min(val)))
         }
 
-        best_wind[[i]] <- c(wind, round(max(val)))*n # weight them by count in order to average the windows
+        best_wind[[i]] <- lapply(un_wind, "*", n) # weight them by count in order to average the windows
       }
 
       n = nrow(data)
       # average them
+      best_wind <- lapply(best_wind, function(x) t(data.frame(x)))
       best_wind <- Reduce("+", best_wind)
       best_wind <- best_wind/n
-      #get them
-      best_wind <- c(floor(min(data$PrecursorMz)), round(best_wind[-c(1, n_window+1)]), ceiling(max(data$PrecursorMz)))
+      best_wind <- unname(apply(best_wind, 1, c, simplify = FALSE))
 
       data$bins <- rep(NA, n)
-      for(i in 1:n_window){# write in the data
-        down <- best_wind[i]
-        up <- best_wind[i+1]
-        idx <- which(data$PrecursorMz >= down & data$PrecursorMz < up)
-        data$bins[idx] <- paste0(down, "-", up)
+      for(i in best_wind){ # get the windows --> same amount for each
+        data$bins[which(data$PrecursorMz >= i[1] & data$PrecursorMz < i[2])] <- paste(i, collapse = "-")
       }
     }
     else{
@@ -165,30 +168,66 @@ get_bestwind <- function(data, n_window = 25, per_frac = FALSE, window_size = NU
         config = append(config, nb)
       }
 
+      config <- c(0, cumsum(config))
+
+      un_wind <- list()
+      for(i in 1:(length(config) - 1)){
+        wind <- range(data$PrecursorMz[(config[i]+1):config[i+1]], na.rm = TRUE)
+        wind[1] <- floor(wind[1])
+        wind[2] <- ceiling(wind[2])
+        un_wind[[i]] <- wind
+      }
+      for(i in 1:(length(un_wind) - 1)){
+        if(un_wind[[i]][2] == un_wind[[length(un_wind)]][2]){
+          un_wind[[i]][2] <- un_wind[[length(un_wind)]][1]
+        }
+        if(un_wind[[i]][2] != un_wind[[i+1]][1]){
+          un_wind[[i+1]][1] <- un_wind[[i]][2]
+        }
+      }
+
       data$bins <- rep(NA, n)
-      for(i in 1:n_window){ # get the windows --> same amount for each
-        if(i == 1){
-          dep = 1
-        }
-        else{
-          dep = sum(config[1:(i-1)])
-        }
-        val = data$PrecursorMz[dep:sum(config[1:i])]
-        if(i == 1){
-          data$bins[dep:sum(config[1:i])] <- paste0(floor(min(val)), "-", round(max(val)))
-        }
-        else if(i == n_window){
-          data$bins[dep:sum(config[1:i])] <- paste0(round(min(val)), "-", ceiling(max(val)))
-        }
-        else{
-          data$bins[dep:sum(config[1:i])] <- paste0(round(min(val)), "-", round(max(val)))
-        }
+      for(i in un_wind){ # get the windows --> same amount for each
+        data$bins[which(data$PrecursorMz >= i[1] & data$PrecursorMz < i[2])] <- paste(i, collapse = "-")
       }
     }
   }
 
-  # print best windows
+  # use overlap window
   un_wind <- unique(data$bins)
+  if(is.numeric(overlap)){
+    overlap <- abs(overlap)
+    if(overlap > 0){
+      mi <- min(as.numeric(unlist(strsplit(un_wind, "-"))))
+
+      if(overlap > mi){
+        message("The overlap you selected is greater than the minimal Precursor mz ! No overlap were applied.")
+      }
+      else{
+        ma <- max(as.numeric(unlist(strsplit(un_wind, "-"))))
+        un_wind <- sapply(un_wind,
+                          function(x){
+                            x <- as.numeric(strsplit(x, "-")[[1]])
+                            if(x[1] > mi){
+                              x[1] <- x[1] - overlap/2
+                            }
+                            if(x[2] < ma){
+                              x[2] <- x[2] + overlap/2
+                            }
+                            x <- paste0(x, collapse = "-")
+                          })
+
+        bins_overlap <- list()
+        for(i in un_wind){
+          new_win <- as.numeric(strsplit(i, "-")[[1]])
+          new_data <- data[which(data$PrecursorMz >= new_win[1] & data$PrecursorMz < new_win[2]),]
+          new_data$bins <- i
+          bins_overlap[[i]] <- new_data
+        }
+        data <- as.data.frame(Reduce(rbind, bins_overlap))
+      }
+    }
+  }
   message(paste("The best windows are", paste(un_wind, collapse = ", ")))
 
   # save plots
